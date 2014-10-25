61c90c6c5f483163252da0792680738b2e755534
PCEgg <ckhk0621@gmail.com>
2014-10-25 21:38:17 +0800

    Setup JSON, Collection View and image

---
+++ 魔靈資訊/PetsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PetsCollectionViewCell.swift
+//  魔靈資訊
+//
+//  Created by CK Lam on 25/10/14.
+//  Copyright (c) 2014 CK Lam. All rights reserved.
+//
+
+import UIKit
+
+class PetsCollectionViewCell: UICollectionViewCell {
+    
+}
